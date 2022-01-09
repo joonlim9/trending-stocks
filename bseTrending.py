1bba1da046744b6882e6f98287d9c7b426ba549a
joonlim9 <joonlim@umich.edu>
2022-01-09 14:07:33 -0500

    change_percentage + dictionary of all company info

--- bseTrending.py
+++ bseTrending.py
@@ -8,17 +8,17 @@ soup = bs4.BeautifulSoup(data, features="html.parser")
 allCompanies = soup.find_all('span', class_='gld13 disin')
 
 # company list
-allCompaniesList = []
+companiesList = []
 for company in allCompanies:
     link = company.find('a').text
-    allCompaniesList.append(link)
+    companiesList.append(link)
 
 
 tableRow = soup.find_all('tr')
 companyLow = []
 companyClose = []
 companyHigh = []
-
+companyChange = []
 for tr in tableRow:
     # high price
     high = tr.find_all('td', attrs={'width':175, 'align': 'right'})
@@ -30,7 +30,7 @@ for tr in tableRow:
         break
 
     # low price
-    low = tr.find_all('td', attrs={'widht': 180, 'align': 'right'})
+    low = tr.find_all('td', attrs={'width': 180, 'align': 'right'})
     for i in low:
         i = i.text.replace(',', '')
         companyLow.append(float(i))
@@ -40,4 +40,23 @@ for tr in tableRow:
     for i in close:
         i = i.text.replace(',', '')
         companyClose.append(float(i))
-    
+
+    # change of price
+    change = tr.find_all('td', attrs={'width': 175, 'align': 'right'})
+    for i in change:
+        if i.has_attr('class'):
+            companyChange.append(float(i.text))
+
+
+companyData = []
+
+for i in range(len(companiesList)):
+    companyData.append({
+        'company': companiesList[i],
+        'high_price': companyHigh[i],
+        'low_price': companyLow[i],
+        'change_percentage': companyChange[i],
+        'close_price': companyClose[i]
+    })
+
+print(companyData)
