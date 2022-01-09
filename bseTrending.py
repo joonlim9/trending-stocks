02012d0d66ee62a7de2e6a37b1e23d973c5cf53e
joonlim9 <joonlim@umich.edu>
2022-01-09 13:55:27 -0500

    high, low, and close price

--- bseTrending.py
+++ bseTrending.py
@@ -7,17 +7,20 @@ soup = bs4.BeautifulSoup(data, features="html.parser")
 
 allCompanies = soup.find_all('span', class_='gld13 disin')
 
+# company list
 allCompaniesList = []
 for company in allCompanies:
     link = company.find('a').text
     allCompaniesList.append(link)
 
-tableRow = soup.find_all('tr')
 
+tableRow = soup.find_all('tr')
 companyLow = []
 companyClose = []
 companyHigh = []
+
 for tr in tableRow:
+    # high price
     high = tr.find_all('td', attrs={'width':175, 'align': 'right'})
     if len(high) == 0:
         continue
@@ -27,8 +30,14 @@ for tr in tableRow:
         break
 
     # low price
-    low = tr.find_all('td', attrs={'width':180, 'align':'right'})
-     
+    low = tr.find_all('td', attrs={'widht': 180, 'align': 'right'})
     for i in low:
         i = i.text.replace(',', '')
-        print(i)
+        companyLow.append(float(i))
+
+    # close price (last price)
+    close = tr.find_all('td', attrs={'width':185, 'align': 'right'})
+    for i in close:
+        i = i.text.replace(',', '')
+        companyClose.append(float(i))
+    
